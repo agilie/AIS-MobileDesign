53935d4c828a6e718cf7c9c2b0d108a5268da9a2
SergeyMell <sergey.mell@agilie.com>
2017-07-25 18:00:46 +0300

    Added BrowserSync and Revisions

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,37 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const del = require('del');
 const runSequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
+const filter = require('gulp-filter');
+const historyApiFallback = require('connect-history-api-fallback');
 
 const $ = require('gulp-load-plugins')();
+
+gulp.task('browser', function() {
+
+    browserSync.init({
+        server: './',
+        middleware: [ historyApiFallback() ],
+        files: ['index.html', 'app/**/*']
+    });
+});
+
+const rev = require('gulp-rev');
+const revReplace = require('gulp-rev-replace');
+
+gulp.task('rev', ['cool-build'], function() {
+
+    const f1 = filter(['**/*', '!dist/index.html'], {restore: true});
+
+    gulp.src(['./dist/**/*.js', 'dist/**/*.css', 'dist/index.html'])
+
+        .pipe(f1).pipe(rev())
+
+        .pipe(f1.restore).pipe(revReplace())
+
+        .pipe(gulp.dest('public'));
+});
+
 
 gulp.task('build', function(done) {
     runSequence(['clean', 'vet'], 'build_js', 'watcher', done);
@@ -48,3 +77,4 @@ gulp.task('build_js', function() {
 gulp.task('watcher', function() {
     return gulp.watch('app/**/*.js', ['build_js']);
 });
+
